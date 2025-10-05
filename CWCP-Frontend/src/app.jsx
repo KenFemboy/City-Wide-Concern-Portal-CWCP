42245ac3a3fb3307e485f13286d0e619dca47c23
Ken Labrador <124577578+KenFemboy@users.noreply.github.com>
2025-10-05 14:19:05 +0800

    added loading screen

--- CWCP-Frontend/src/app.jsx
+++ CWCP-Frontend/src/app.jsx
@@ -1,48 +1,63 @@
+import { useState, useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Elements
 import Main from "./MAINPAGE/main.jsx";
-
 import Searchbar from "./SEARCHBAR/searchbar.jsx";
 import Mod from "./MODERATOR/login.jsx";
+import Credits from "./Credits/credits.jsx";
+import TestLinks from "./Credits/testlinks.jsx";
+import LoadingScreen from "./LoadingScreen/loadingscreen.jsx"
 
-import Credits from "./Credits/credits.jsx"
-import TestLinks from "./Credits/testlinks.jsx"
+
 function App() {
-    const route = createBrowserRouter([
-        {
-            path: "/",
-            element: (
-                <>
-                    <div id="main">
-                        <Main />
-                    </div>
-                    <Credits/>
-                    <TestLinks/>
-                </>
-            ),
-        },
-        {
-            path: "/mod",
-            element: <Mod />,
-        },
-        {
-            path: "/dashboard",
-            element:
-                <>
-                    <div id="main">
-                        <Searchbar />
-                        <Main />
-                    </div>
+  const [loading, setLoading] = useState(true);
 
-                </>
-            ,
-        },
-    ]);
-    return (
-        <div>
-            <RouterProvider router={route} />
-        </div>
-    );
+  
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000); 
+    return () => clearTimeout(timer);
+  }, []);
+
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <div id="main">
+            <Main />
+          </div>
+          <Credits />
+          <TestLinks />
+        </>
+      ),
+    },
+    {
+      path: "/mod",
+      element: <Mod />,
+    },
+    {
+      path: "/dashboard",
+      element: (
+        <>
+          <div id="main">
+            <Searchbar />
+            <Main />
+          </div>
+        </>
+      ),
+    },
+  ]);
+
+  return (
+    <div>
+      {loading ? (
+        <LoadingScreen/>
+      ) : (
+        <RouterProvider router={route} />
+      )}
+    </div>
+  );
 }
+
 export default App;
