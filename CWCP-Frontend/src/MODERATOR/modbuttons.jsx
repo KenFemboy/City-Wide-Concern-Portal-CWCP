9cccb19d803b3aac1061d75b3be97f37fff8d1e0
Ken Labrador <124577578+KenFemboy@users.noreply.github.com>
2025-10-09 19:21:23 +0800

    finalize layout and added responsiveness

--- CWCP-Frontend/src/MODERATOR/modbuttons.jsx
+++ CWCP-Frontend/src/MODERATOR/modbuttons.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Navigate, useNavigate } from 'react-router-dom'
+import "./modbuttons.css"
 const modbuttons = () => {
   const navigate = useNavigate();
 
   return (
     <div>
-      <button
-        alt="CWCP Logo"
-        onClick={() => navigate("/mod")}
-      >Moderator Login</button>
+      <div className="mod-buttons">
+  <button onClick={() => navigate("/mod")}>Moderator Login</button>
+  
+</div>
     </div>
   )
 }
